70c32cf964eaf579a257367853630abbe41bf1f0
LIUCBK <liucbk@bc.edu>
2025-01-21 14:41:18 -0500

    Updates after video 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,28 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var message = "I AM A Programmer!"
+    @State private var message = ""
+    @State private var imageString = ""
     var body: some View {
         VStack {
             Spacer()
-            Image(systemName: "swift")
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.thin)
                 .foregroundStyle(.black)
             Spacer()
             
-            HStack{
-                Button("Awesome") {
-                    message = "Awesome!"
-                }
-                Button("Great"){
-                    message = "Great!"
-                }
+            
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+//                if message == message1{
+//                    message = message2
+//                    imageString = imageString2
+//                }else{
+//                    message = message1
+//                    imageString = imageString1
+//                }
+                message = (message == message1 ? message2 : message1)
+                imageString = (imageString == imageString1 ? imageString2 : imageString1)
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
